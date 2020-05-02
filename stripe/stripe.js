1db031999e6ff001f135b56cb87b220503e992e1
riberahugo <lesheitan.bitcoin@gmail.com>
2020-05-02 13:45:56 +0200

    V1.1

--- stripe/stripe.js
+++ stripe/stripe.js
@@ -10,41 +10,104 @@ router.use(bodyParser.json());
 
 // var price;
 
-// const stripe = require('stripe')('sk_test_dn1JDGQuqnnql3fTAk469Pnj00MOIs38kS');
+const stripe = require('stripe')('sk_test_dn1JDGQuqnnql3fTAk469Pnj00MOIs38kS');
 
-// router.post('/', VerifyToken, function(req, res, next) {
+router.post('/', function(req, res, next) {
 
-//     Course.getCourseById(req.body.price, function(err, group) {
+    var amount = req.body;
 
-//     if (err) {
-//         return res.status(500).send({ error: 'Something failed!' })
-//     }
-//     else {
-
-//         if(group[0].reduction == 0){
-//             this.price = group[0].prix * 100
-//         }
-//         else{
-//             this.price = group[0].prix - (group[0].prix * group[0].reduction)/100
-//             this.price = this.price * 100
-//         }
-
-//         stripe.charges.create(
-//             {
-//               amount: this.price,
-//               currency: 'eur',
-//               source: 'tok_amex',
-//               description: 'Paiement d\'une formation LMS',
-//             },
-//             function(err, charge) {
-              
-//             }
+    if(amount.type == 'debutant'){
+        console.log('paiement pack débutant')
+        stripe.charges.create(
+            {
+                amount: 26000,
+                currency: 'eur',
+                source: 'tok_amex',
+                description: 'Paiement d\'un pack débutant',
+            },
+            function(err, charge) {
+                console.log(charge)
+            }
             
-//         );
-//         return res.status(200).send(group);
-//     }
-//     });
+        );
+    }
+    if(amount.type == 'pro'){
+        console.log('paiement pack pro')
+        stripe.charges.create(
+            {
+                amount: 20000,
+                currency: 'eur',
+                source: 'tok_amex',
+                description: 'Paiement d\'un pack pro',
+            },
+            function(err, charge) {
+                console.log(charge)
+            }
+            
+        );
+    }
+});
 
-// });
+router.post('/abonnement/:plan', function(req, res, next) {
+
+    var token = req.body;
+    var plan = req.params.plan;
+
+    console.log(token)
+    console.log(token.email)
+
+    stripe.customers.create(
+        {
+          source: token.id,
+          email: token.email
+        //description:'hello'
+        },
+        function(err, customer) {
+            // asynchronously called
+
+            var cancelatDebutant = (Date.now() / 1000) + 13 * 604800 // 13 semaines
+            cancelatDebutant = Math.round(cancelatDebutant)
+
+            var cancelatPro = (Date.now() / 1000) + 10 * 604800 // 10 semaines
+            cancelatPro = Math.round(cancelatPro)
+
+            if(plan == 'debutant'){
+
+                stripe.subscriptions.create({
+                    customer: customer.id,
+                    //cancel_at_period_end: true,
+                    cancel_at: cancelatDebutant, //nb de sconde dans une semaine
+                    items: [
+                        {
+                            plan: 'plan_HCifskulHJ6uev'
+                        }
+                    ]
+                },
+                function(err, subscriptions) {
+                    console.log(subscriptions)
+                }
+                );
+            }
+            if(plan == 'pro'){
+
+                stripe.subscriptions.create({
+                    customer: customer.id,
+                    //cancel_at_period_end: true,
+                    cancel_at: cancelatPro, //nb de sconde dans une semaine
+                    items: [
+                        {
+                            plan: 'plan_HCifskulHJ6uev'
+                        }
+                    ]
+                },
+                function(err, subscriptions) {
+                    console.log(subscriptions)
+                }
+                );
+            }
+
+          }
+    );
+});
 
 module.exports = router;
