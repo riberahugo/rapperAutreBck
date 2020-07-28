5b4d3335f0d6ff817d1070b0cb2198f176333dcc
riberahugo <lesheitan.bitcoin@gmail.com>
2020-07-28 04:22:49 +0200

    bb

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ global.__root   = __dirname + '/';
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
+//app.use(express.bodyParser({limit: '50mb'}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 
 app.use(express.static('./dist/'));
